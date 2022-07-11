d30b81d829615e45839afb0ec84ced4147ed1aaf
MrShirokow <mr.shirokow@gmail.com>
2022-07-11 19:26:20 +0500

    create utils.py file

---
+++ skyeng/internal/utils.py
@@ -0,0 +1,56 @@
+import base64
+import functools
+import hashlib
+import hmac
+import io
+
+
+def api_secret_check(request_function):
+
+    @functools.wraps(request_function)
+    def request_wrapper(self, request, *args, **kwargs):
+        if request.headers.get('X-Signature') != get_signature(request, API_SECRET):
+            return HttpResponseForbidden('Unknown API key')
+        return request_function(self, request, *args, **kwargs)
+
+    return request_wrapper
+
+
+def get_signature(request, key):
+    request_str = f'{request.method}\n{request.path}'
+    return hmac.new(key.encode(), request_str.encode(), hashlib.sha256).hexdigest()
+
+
+def positive_int(integer_string, strict=False, cutoff=None):
+    number = int(integer_string)
+    if number < 0 or (number == 0 and strict):
+        raise ValueError()
+    if cutoff:
+        return min(number, cutoff)
+    return number
+
+
+def get_limit(query_params, default=50):
+    try:
+        return positive_int(query_params.get('limit'),
+                            strict=True,
+                            cutoff=100)
+    except (TypeError, ValueError):
+        return default
+
+
+def get_offset(query_params):
+    try:
+        return positive_int(query_params.get('offset'))
+    except (TypeError, ValueError):
+        return 0
+
+
+def paginate(query_params, query_set):
+    offset = get_offset(query_params)
+    limit = get_limit(query_params)
+    return query_set[offset:offset + limit]
+
+
+def decode_file(encoded_data: str):
+    return io.BytesIO(base64.b64decode(encoded_data.encode('utf-8')))
--- skyeng/internal/views.py
+++ skyeng/internal/views.py
@@ -1,11 +1,7 @@
-import base64
-import functools
-import hashlib
-import hmac
 import json
-import io
 
 import skyeng.internal.serializers as serializers
+import skyeng.internal.utils as utils
 
 from django.db import connection
 from django.core.files import File
@@ -20,70 +16,19 @@ from skyeng.internal.models.word.model import Word
 from skyeng.internal.models.word.form import WordForm
 
 
-def api_secret_check(request_function):
-
-    @functools.wraps(request_function)
-    def request_wrapper(self, request, *args, **kwargs):
-        # if request.headers.get('X-Signature') != get_signature(request, API_SECRET):
-        #     return HttpResponseForbidden('Unknown API key')
-        return request_function(self, request, *args, **kwargs)
-
-    return request_wrapper
-
-
-def get_signature(request, key):
-    request_str = f'{request.method}\n{request.path}'
-    return hmac.new(key.encode(), request_str.encode(), hashlib.sha256).hexdigest()
-
-
-def positive_int(integer_string, strict=False, cutoff=None):
-    number = int(integer_string)
-    if number < 0 or (number == 0 and strict):
-        raise ValueError()
-    if cutoff:
-        return min(number, cutoff)
-    return number
-
-
-def get_limit(query_params, default=50):
-    try:
-        return positive_int(query_params.get('limit'),
-                            strict=True,
-                            cutoff=100)
-    except (TypeError, ValueError):
-        return default
-
-
-def get_offset(query_params):
-    try:
-        return positive_int(query_params.get('offset'))
-    except (TypeError, ValueError):
-        return 0
-
-
-def paginate(query_params, query_set):
-    offset = get_offset(query_params)
-    limit = get_limit(query_params)
-    return query_set[offset:offset + limit]
-
-
-def decode_file(encoded_data: str):
-    return io.BytesIO(base64.b64decode(encoded_data.encode('utf-8')))
-
-
 class CategoryListView(View):
 
-    @api_secret_check
+    @utils.api_secret_check
     def get(self, request):
         query_set = Category.objects.all()
         query_params = request.GET
-        query_set = serializers.serialize_category_list(request, paginate(query_params, query_set))
+        query_set = serializers.serialize_category_list(request, utils.paginate(query_params, query_set))
         return JsonResponse(query_set, safe=False)
 
-    @api_secret_check
+    @utils.api_secret_check
     def post(self, request):
         request_body = json.loads(request.body)
-        file_content = decode_file(request_body.get('icon'))
+        file_content = utils.decode_file(request_body.get('icon'))
         request_files = {'icon': ImageFile(file_content, name='icon.jpg')}
         category_form = CategoryForm(request_body, request_files)
 
@@ -97,7 +42,7 @@ class CategoryListView(View):
 
 class ThemeListView(View):
 
-    @api_secret_check
+    @utils.api_secret_check
     def get(self, request):
         query_params = request.GET
         query_set = Theme.objects.all()
@@ -108,13 +53,13 @@ class ThemeListView(View):
         if level:
             query_set = query_set.filter(level=level)
 
-        query_set = serializers.serialize_theme_list(request, paginate(query_params, query_set))
+        query_set = serializers.serialize_theme_list(request, utils.paginate(query_params, query_set))
         return JsonResponse(query_set, safe=False)
 
-    @api_secret_check
+    @utils.api_secret_check
     def post(self, request):
         request_body = json.loads(request.body)
-        file_content = decode_file(request_body.get('photo'))
+        file_content = utils.decode_file(request_body.get('photo'))
         request_files = {'photo': ImageFile(file_content, name='photo.jpg')}
 
         theme_form = ThemeForm(request_body, request_files)
@@ -134,7 +79,7 @@ class ThemeListView(View):
 
 class ThemeDetailView(View):
 
-    @api_secret_check
+    @utils.api_secret_check
     def get(self, request, theme_id):
         theme = Theme.objects.filter(pk=theme_id).select_related('category').prefetch_related('words').first()
         if not theme:
@@ -147,7 +92,7 @@ class ThemeDetailView(View):
 
 class LevelDetailView(View):
 
-    @api_secret_check
+    @utils.api_secret_check
     def get(self, request):
         items_data = serializers.serialize_level(Level)
         return JsonResponse(items_data, safe=False)
@@ -155,7 +100,7 @@ class LevelDetailView(View):
 
 class WordDetailView(View):
 
-    @api_secret_check
+    @utils.api_secret_check
     def get(self, request, word_id):
         word = Word.objects.filter(id=word_id).first()
         if not word:
@@ -167,17 +112,17 @@ class WordDetailView(View):
 
 class WordListView(View):
 
-    @api_secret_check
+    @utils.api_secret_check
     def get(self, request):
         query_set = Word.objects.all()
         query_params = request.GET
-        query_set = serializers.serialize_word_list(paginate(query_params, query_set))
+        query_set = serializers.serialize_word_list(utils.paginate(query_params, query_set))
         return JsonResponse(query_set, safe=False)
 
-    @api_secret_check
+    @utils.api_secret_check
     def post(self, request):
         request_body = json.loads(request.body)
-        file_content = decode_file(request_body.get('sound'))
+        file_content = utils.decode_file(request_body.get('sound'))
         request_files = {'sound': File(file_content, name='sound.mp3')}
         word_form = WordForm(request_body, request_files)
         if not word_form.is_valid():
