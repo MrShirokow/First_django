a5623db4013e98ca53c3b031a1b750e7e32f6328
MrShirokow <mr.shirokow@gmail.com>
2022-07-15 16:42:42 +0500

    change api_secret middleware to non-global

--- skyeng/api_secret_middleware.py
+++ skyeng/api_secret_middleware.py
@@ -1,7 +1,11 @@
 from django.core.handlers.wsgi import WSGIRequest
 from django.http import HttpResponseForbidden
+from django.urls import resolve
+
 from config.settings import API_SECRET
 from skyeng.internal import utils
+from skyeng.internal.views import CategoryListView, ThemeListView, ThemeDetailView, \
+                                  LevelDetailView, WordListView, WordDetailView
 
 
 class ApiSecretMiddleware:
@@ -12,6 +16,9 @@ class ApiSecretMiddleware:
         self.get_response = get_response
 
     def __call__(self, request: WSGIRequest):
-        if request.headers.get('X-Signature') != utils.get_signature(request, API_SECRET):
-            return HttpResponseForbidden('Unknown API key')
+        classes = [CategoryListView, ThemeListView, ThemeDetailView, LevelDetailView, WordListView, WordDetailView]
+        view_func, view_args, view_kwargs = resolve(request.path)
+        if hasattr(view_func, 'view_class') and view_func.view_class in classes:
+            if request.headers.get('X-Signature') != utils.get_signature(request, API_SECRET):
+                return HttpResponseForbidden('Unknown API key')
         return self.get_response(request)
